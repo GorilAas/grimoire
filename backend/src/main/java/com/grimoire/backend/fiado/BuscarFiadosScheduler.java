a4e2b62996b6e60570aefc3c02d5c397c2bc1e84
Lucas Camacho <lucasbaratamc@gmail.com>
2026-05-14 18:39:19 -0300

    Ajuste caso de uso

--- backend/src/main/java/com/grimoire/backend/fiado/BuscarFiadosScheduler.java
+++ backend/src/main/java/com/grimoire/backend/fiado/BuscarFiadosScheduler.java
@@ -1,13 +1,13 @@
 package com.grimoire.backend.fiado;
 
 import com.grimoire.backend.notificacao.NotificacaoModel;
+import com.grimoire.backend.shared.enums.StatusFiado;
 import com.grimoire.backend.venda.Venda;
 import com.grimoire.backend.venda.VendaRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.scheduling.annotation.Scheduled;
-
 import java.util.List;
 
 @Slf4j
@@ -19,7 +19,7 @@ public class BuscarFiadosScheduler {
 
     @Scheduled(cron = "${schedule.buscar-fiados.cron}")
     public void buscarFiados() {
-        List<Venda> vendas = vendaRepository.listarTodas();
+        List<Venda> vendas = vendaRepository.listarPorStatusFiado(StatusFiado.PENDENTE);
 
         vendas.forEach(venda -> {
             //Aqui dentro o ideal seria chamar um port de integração de Whatsapp para enviar notificações diretamente
